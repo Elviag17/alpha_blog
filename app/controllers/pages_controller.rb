b3b754b3bca52f30ced20a59d9ec2596d13202f4
Elvia Gutierrez <elviagutierrez07@gmail.com>
2021-02-04 00:00:42 -0800

    added articles table and migration files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    def pages
+    def home
     end
 
     def about
---
+++ db/migrate/20210204075739_add_timestamps_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimestampsToArticles < ActiveRecord::Migration[6.1]
+  def change
+
+    # action, to which table, column name, data type
+   
+  end
+end
