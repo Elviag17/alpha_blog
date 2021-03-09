e4ba1ba9133d72bfd66dd8a0c69a0d8b5caee28f
Elvia Gutierrez <elviagutierrez07@gmail.com>
2021-03-08 23:26:49 -0800

    added edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ root 'pages#home'
 
 get 'about', to:'pages#about'
 
-resources :articles, only: [:show, :index]
+resources :articles, only: [:show, :index, :new, :create , :edit, :update]
 
 end
+  
